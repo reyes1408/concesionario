bd68cd7883ac1b91b1574cbd12933f7d7963ee31
Alavazarez2004 <145495596+Alavazarez2004@users.noreply.github.com>
2023-11-01 22:28:25 -0600

    Se creo las rutas en el archivo auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,15 @@ import { registrarVendedor } from "../controllers/vendedor.controller.js";
 import { registrarVehiculo } from "../controllers/vehiculo.controller.js";
 import { registrarVenta } from "../controllers/ventas.controller.js";
 
+import {actualizarVendedor} from "../controllers/vendedor.controller.js";
+import {actualizarVehiculo} from "../controllers/vehiculo.controller.js";
+
+import {borrarVendedor} from "../controllers/vendedor.controller.js";
+import {borrarVehiculo} from "../controllers/vehiculo.controller.js";
+
+import {buscarVendedor} from "../controllers/vendedor.controller.js";
+import {buscarVehiculo} from "../controllers/vehiculo.controller.js";
+
 const router = Router();
 
 //RUTAS
@@ -13,14 +22,15 @@ router.post('/cliente/registrar', registrarCliente);  //GABO
 // router.get('/cliente/buscar/:id', findUser);
 
 router.post('/vendedor/registrar', registrarVendedor); //MIGUE
-// router.put('/vendedor/actualizar/:id', updateUser);
-// router.delete('/vendedor/borrar/:id', deleteUser);
-// router.get('/vendedor/buscar/:id', findUser);
+router.put('/vendedor/actualizar/:id', actualizarVendedor);
+router.delete('/vendedor/borrar/:id', borrarVendedor);
+router.post('/vendedor/buscar/:id', buscarVendedor);
 
 router.post('/vehiculo/registrar', registrarVehiculo); //MIGUE
-// router.put('/vehiculo/actualizar/:id', updateUser);
-// router.delete('/vehiculo/borrar/:id', deleteUser);
-// router.get('/vehiculo/buscar/:id', findUser);
+router.put('/vehiculo/actualizar/:id', actualizarVehiculo);
+router.delete('/vehiculo/borrar/:id', borrarVehiculo);
+router.get('/vehiculo/buscar/:id', buscarVehiculo);
+
 
 router.post('/ventas/registrar', registrarVenta); //GABO
 // router.put('/ventas/actualizar/:id', updateUser);
