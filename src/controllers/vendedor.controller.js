c22c8ef25583f51d9d6468747a7ee4c27b92a7f9
Alavazarez2004 <145495596+Alavazarez2004@users.noreply.github.com>
2023-11-01 22:26:47 -0600

    Se creo el archivo vendedor.controller.js

--- src/controllers/vendedor.controller.js
+++ src/controllers/vendedor.controller.js
@@ -26,3 +26,62 @@ export const registrarVendedor = async (req, res) => {
     }
 
 };
+
+export const actualizarVendedor = async (req, res) => {
+    const idVendedor = req.params.id;
+    const newData = req.body;
+
+    try {
+        const vendedor = await vendedorModel.findByIdAndUpdate(idVendedor, newData, { new: true });
+
+        if (vendedor) {
+            console.log("Vendedor actualizado: ", vendedor);
+            res.status(200).json({ message: "Vendedor actualizado correctamente", vendedor });
+        } else {
+            res.status(404).json({ message: "Vendedor no encontrado" });
+        }
+    } catch (error) {
+        console.error("Error al actualizar", error);
+        res.status(500).json({ message: "Error al actualizar el vendedor" });
+    }
+}
+
+export const borrarVendedor = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const vendedorDeleted = await vendedorModel.findByIdAndDelete(id);
+
+        if (vendedorDeleted) {
+            console.log("Vendedor eliminado", vendedorDeleted);
+            res.status(200).json({ message: "Vendedor eliminado correctamente", vendedorDeleted });
+        } else {
+            console.log("No se encontró un vendedor con este ID");
+            res.status(404).json({ message: "No se encontró un vendedor con este ID" });
+        }
+    } catch (error) {
+        console.log("Error al intentar eliminar al vendedor ", error);
+        res.status(500).json({ message: "Error al intentar eliminar al vendedor" });
+    }
+};
+
+export const buscarVendedor = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+
+        const sellerFound =  await vendedorModel.findById(id);
+
+        if (sellerFound) {
+            console.log("Vendedor encontrado: ", sellerFound);
+            res.status(200).json({mensaje: "Vendedor encontrado: ", sellerFound});
+        } else {
+            console.log("Vendedor no encontrado");
+            res.status(404).json({mensaje: "Vendedor no encontrado"});            
+        }
+        
+    } catch (error) {
+        console.log("Error al intentar buscar al vendedor ", error);
+        res.status(500).json({ message: "Error al intentar buscar al vendedor" });
+    }
+};
